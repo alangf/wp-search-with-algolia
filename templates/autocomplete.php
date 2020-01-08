78dd35432454900279f242957bcc84e0685081cb
Alan Gomes <alangf@gmail.com>
2020-01-07 22:59:18 -0300

    remove term icon, add term description

--- templates/autocomplete.php
+++ templates/autocomplete.php
@@ -20,15 +20,11 @@
 </script>
 
 <script type="text/html" id="tmpl-autocomplete-term-suggestion">
-  <a class="suggestion-link" href="{{ data.permalink }}" title="{{ data.name }}">
-	<svg viewBox="0 0 21 21" width="21" height="21">
-	  <svg width="21" height="21" viewBox="0 0 21 21">
-		<path
-			d="M4.662 8.72l-1.23 1.23c-.682.682-.68 1.792.004 2.477l5.135 5.135c.7.693 1.8.688 2.48.005l1.23-1.23 5.35-5.346c.31-.31.54-.92.51-1.36l-.32-4.29c-.09-1.09-1.05-2.06-2.15-2.14l-4.3-.33c-.43-.03-1.05.2-1.36.51l-.79.8-2.27 2.28-2.28 2.27zm9.826-.98c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25z"
-			fill-rule="evenodd"></path>
-	  </svg>
-	</svg>
+  <a class="suggestion-link" href="{{ data.permalink }}" title="{{ data.name }}">{{data.images}}
 	<span class="suggestion-post-title">{{{ data._highlightResult.name.value }}}</span>
+		  <# if ( data._highlightResult.description.value ) { #>
+			<span class="suggestion-post-content">{{{ data._highlightResult.description.value }}}</span>
+			<# } #>
   </a>
 </script>
 
@@ -94,7 +90,7 @@
 		source: algoliaAutocomplete.sources.hits(client.initIndex(config['index_name']), {
 		  hitsPerPage: config['max_suggestions'],
 		  attributesToSnippet: [
-			'content:10'
+			'content:20'
 		  ],
 		  highlightPreTag: '__ais-highlight__',
 		  highlightPostTag: '__/ais-highlight__'
@@ -110,7 +106,6 @@
         return suggestion_template(hit);
       }
       hit.escaped = true;
-
 			for (var key in hit._highlightResult) {
 			  /* We do not deal with arrays. */
 			  if (typeof hit._highlightResult[key].value !== 'string') {
